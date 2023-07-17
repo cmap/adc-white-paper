5ddf9e64d3203393b267ad9e91de8f19592696b7
Lia Petronio <lpetroni@broadinstitute.org>
2023-07-17 16:06:11 -0400

    revert to Home naming

--- src/router/index.js
+++ src/router/index.js
@@ -8,11 +8,11 @@ const routes = [
     children: [
       {
         path: '',
-        name: 'AntibodyDrugCongugatersPaper',
+        name: 'Home',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/AntibodyDrugCongugatersPaper.vue'),
+        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
       },
     ],
   },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -218,7 +218,7 @@
 
   const dataPath = "../../public/data/";
   export default {
-        name: 'AntibodyDrugCongugatersPaper',
+        name: 'Home',
         components: {PaperHeader, PaperSection, PaperSubSection, ImageCard},
         data () {
           return {
